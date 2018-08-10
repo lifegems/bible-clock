a3242895c993b4cb80d5d8e13013bdf69c9f9964
Michalik, Joshua <jsmichal@usa.wtbts.net>
2018-08-10 17:07:36 -0400

    Show bible book on progress

--- progress.js
+++ progress.js
@@ -21,34 +21,35 @@ function formatVerse(citation) {
 function getPercentage(part, total) {
    return (part / (total / 100)).toFixed(2) + '%';
 }
-function initFormatted() {
-   var formatted = {
+function initChapters() {
+   var chapters = {
        0: [],  1: [],  2: [],  3: [],  4: [],  5: [],
        6: [],  7: [],  8: [],  9: [], 10: [], 11: [],
       12: [], 13: [], 14: [], 15: [], 16: [], 17: [],
       18: [], 19: [], 20: [], 21: [], 22: [], 23: [],
    };
-   return formatted;
+   return chapters;
 }
 
 csv({delimiter: '|'})
    .fromFile(csvFile)
    .then((data) => {
-      const formatted = initFormatted();
+      const chapters = initChapters();
+      const books = initChapters();
       data.forEach(passage => {
-         var citation = formatVerse(passage.verse);
-         if (!formatted.hasOwnProperty(citation.chapter)) {
-            formatted[citation.chapter] = [];
-         }
-        formatted[citation.chapter].push(citation.verse);
+        var citation = formatVerse(passage.verse);
+        chapters[citation.chapter].push(citation.verse);
+        if (books[citation.chapter].indexOf(passage.book) === -1) {
+            books[citation.chapter].push(passage.book);
+        }
       });
       var totalVerses = data.length;
       data = []; // clearing the object to help with memory
       var totalUniqueVerses = 0;
-      Object.keys(formatted).forEach(chapter => {
-         var uniqueVerses = formatted[chapter].filter((v, i, a) => a.indexOf(v) === i).length;
+      Object.keys(chapters).forEach(chapter => {
+         var uniqueVerses = chapters[chapter].filter((v, i, a) => a.indexOf(v) === i).length;
          totalUniqueVerses += uniqueVerses;
-         console.log(chapter + ': ' + formatted[chapter].length + ' - ' + uniqueVerses + '/60 - ' + getPercentage(uniqueVerses, 60));
+         console.log(chapter + ': ' + chapters[chapter].length + ' - ' + uniqueVerses + '/60 - ' + getPercentage(uniqueVerses, 60) + ' - ' + books[chapter].join(', '));
       });
       console.log("Unique Verses: " + totalUniqueVerses + '/1440 - ' + getPercentage(totalUniqueVerses, 1440));
       console.log("Total Verses:", totalVerses);
