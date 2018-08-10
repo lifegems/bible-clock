e571342572ad1cceba9c7a02b166984cab848352
Michalik, Joshua <jsmichal@usa.wtbts.net>
2018-08-10 13:38:08 -0400

    Add all the placeholders

--- csv_to_json.js
+++ csv_to_json.js
@@ -2,6 +2,27 @@ const csv = require('csvtojson');
 const fs = require('fs');
 const csvFile = 'bible_clock_verses.csv';
 
+function generatePlaceholders() {
+   var hours = [];
+   var minutes = [];
+   for (var i = 0; i < 60; i++) {
+      var count = 100 + i;
+      minutes.push(count.toString().substring(1));
+      if (i < 24) {
+         hours.push(count.toString().substring(1));
+      }
+   }
+   var placeholder = [{"verse":"00-00","book":"","quote":"The New World Translation of the Holy Scriptures"}];
+   hours.forEach(hour => {
+      minutes.forEach(minute => {
+         file = hour + "-" + minute + ".json";
+         fs.writeFileSync('docs/times/' + file, JSON.stringify(placeholder));
+      });
+   });
+}
+
+generatePlaceholders();
+
 csv({delimiter: '|'})
    .fromFile(csvFile)
    .then((data) => {
